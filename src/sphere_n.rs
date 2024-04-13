81fb7ae67773c497387aedbcf12b1cf98e3abc04
Wai-Shing Luk <luk036@gmail.com>
2024-04-13 07:35:07 +0000

    add doc

--- src/sphere_n.rs
+++ src/sphere_n.rs
@@ -10,6 +10,18 @@ lazy_static! {
     static ref X: Array1<f64> = Array1::linspace(0.0, PI, 300);
 }
 
+/// The struct `Gl` in Rust contains three arrays of type `f64` representing `x`, `neg_cosine`, and
+/// `sine`.
+///
+/// Properties:
+///
+/// * `x`: The `x` property in the `Gl` struct appears to be an array of floating-point numbers (`f64`).
+/// It seems to represent some kind of data related to the struct.
+/// * `neg_cosine`: The `neg_cosine` property in the `Gl` struct seems to be an array of floating-point
+/// numbers (`f64`). It likely stores the negative cosine values for some calculations or processing
+/// within the struct.
+/// * `sine`: The `sine` property in the `Gl` struct is an `Array1<f64>` type, which likely represents
+/// an array of floating-point numbers (f64) storing the sine values.
 struct Gl {
     x: Array1<f64>,
     neg_cosine: Array1<f64>,
@@ -24,6 +36,9 @@ lazy_static! {
     };
 }
 
+/// The `SphereGen` trait in Rust defines a set of methods that need to be implemented by types that
+/// want to be considered as generators for spheres. Here's a breakdown of the methods defined in the
+/// `SphereGen` trait:
 pub trait SphereGen {
     // fn new(base: &[usize]) -> Self;
     fn pop_vec(&mut self) -> Vec<f64>;
@@ -31,19 +46,33 @@ pub trait SphereGen {
     fn get_tp(&self) -> &Array1<f64>;
 }
 
+/// The `Sphere3` struct in Rust contains fields for VdCorput, Sphere, and an Array1<f64>.
+///
+/// Properties:
+///
+/// * `vdc`: The `vdc` property in the `Sphere3` struct is of type `VdCorput`.
+/// * `sphere2`: The `sphere2` property in the `Sphere3` struct is of type `Sphere`. It seems to be a
+/// reference to another struct named `Sphere`.
+/// * `tp`: The `tp` property in the `Sphere3` struct is of type `Array1<f64>`, which is an array of
+/// floating-point numbers with one dimension.
 pub struct Sphere3 {
     vdc: VdCorput,
     sphere2: Sphere,
     tp: Array1<f64>,
 }
 
-/** Generate Sphere-3 Halton sequence */
 impl Sphere3 {
-    /**
-     * @brief Construct a new Sphere3 object
-     *
-     * @param base
-     */
+    /// The function `new` constructs a new `Sphere3` object with specified parameters.
+    ///
+    /// Arguments:
+    ///
+    /// * `base`: The `base` parameter is an array of `usize` values that contains information needed to
+    /// initialize a `Sphere3` object. It is used to create a new `Sphere3` object by passing specific
+    /// values to initialize its internal components such as `VdCorput` and `Sphere`.
+    ///
+    /// Returns:
+    ///
+    /// A new `Sphere3` object is being returned from the `new` function.
     pub fn new(base: &[usize]) -> Self {
         Sphere3 {
             vdc: VdCorput::new(base[0]),
@@ -53,6 +82,14 @@ impl Sphere3 {
         }
     }
 
+    /// The `pop` function in Rust calculates values based on input data and returns a 4-element array.
+    ///
+    /// Returns:
+    ///
+    /// The function `pop` returns an array of 4 `f64` values. The first three values are calculated
+    /// based on some operations involving popping values from `self.vdc` and `self.sphere2`, and the
+    /// last value is the cosine of the interpolated value `xi`. The array returned contains the values
+    /// `[sinxi * s0, sinxi * s1, sinxi * s
     pub fn pop(&mut self) -> [f64; 4] {
         let ti = HALF_PI * self.vdc.pop(); // map to [0, pi/2];
         let xi = interp(&self.tp.to_vec(), &X.to_vec(), ti);
@@ -82,29 +119,56 @@ impl Sphere3 {
 /// assert_approx_eq!(res[0], 0.3430622238280562);
 /// ```
 impl SphereGen for Sphere3 {
+    #[inline]
     fn reseed(&mut self, seed: usize) {
         self.vdc.reseed(seed);
         self.sphere2.reseed(seed);
     }
 
+    #[inline]
     fn pop_vec(&mut self) -> Vec<f64> {
         self.pop().to_vec()
     }
 
+    #[inline]
     fn get_tp(&self) -> &Array1<f64> {
         &self.tp
     }
 }
 
-/** Generate Sphere-3 Halton sequence */
+/// The `NSphere` struct represents a generator for Sphere-N Halton sequence.
+///
+/// Properties:
+///
+/// * `vdc`: The `vdc` property seems to be of type `VdCorput`, which is likely used for generating
+/// Halton sequences. Halton sequences are deterministic sequences that are used for quasi-random
+/// sampling. The `VdCorput` struct probably implements the Van der Corput sequence generation
+/// algorithm.
+/// * `s_gen`: The `s_gen` property in the `NSphere` struct is a Box containing a trait object that
+/// implements the `SphereGen` trait. This allows for dynamic dispatch and the ability to store
+/// different types that implement the `SphereGen` trait in the `NSphere` struct.
+/// * `tp`: The `tp` property in the `NSphere` struct is of type `Array1<f64>`. It is used to store some
+/// data related to the sphere generation process.
 pub struct NSphere {
     vdc: VdCorput,
     s_gen: Box<dyn SphereGen>,
     tp: Array1<f64>,
 }
 
-// static IntSinPowerTable sp {};
 impl NSphere {
+    /// The function `new` in Rust initializes a NSphere struct with specific parameters based on the
+    /// input size and base array.
+    ///
+    /// Arguments:
+    ///
+    /// * `n`: The parameter `n` represents the dimensionality of the sphere being generated.
+    /// * `base`: The `base` parameter is a slice of `usize` values that contains the base values used
+    /// for generating the NSphere. The function `new` takes two parameters: `n`, which is the dimension
+    /// of the NSphere, and `base`, which is a slice containing the base values needed for
+    ///
+    /// Returns:
+    ///
+    /// The `new` function returns an instance of the `NSphere` struct.
     pub fn new(n: usize, base: &[usize]) -> Self {
         assert!(n >= 3);
         let (s_gen, tp_minus2): (Box<dyn SphereGen>, Array1<f64>) = if n == 3 {
@@ -124,6 +188,15 @@ impl NSphere {
         }
     }
 
+    /// The function `get_tp_minus1` returns a reference to an Array1<f64> obtained from calling the
+    /// `get_tp` method on the `s_gen` field.
+    ///
+    /// Returns:
+    ///
+    /// The `get_tp_minus1` function is returning a reference to an `Array1<f64>` which is obtained by
+    /// calling the `get_tp` method on the `s_gen` field of the struct or object that the function is
+    /// defined on.
+    #[inline]
     pub fn get_tp_minus1(&self) -> &Array1<f64> {
         self.s_gen.get_tp()
     }
@@ -387,8 +460,6 @@ impl CylindN {
     /**
      * @brief Construct a new cylin n::cylin n object
      *
-     * @param n
-     * @param base
      */
     #[allow(dead_code)]
     pub fn new(n: usize, base: &[usize]) -> Self {
